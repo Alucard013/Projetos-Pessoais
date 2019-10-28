03d152252177381f86ea6ebe11ceca3eb5978e5a
Aluno Bandtec <github@alunos.bandtec.com.br>
2019-10-28 19:54:34 -0200

    up

--- game/js/in_out.js
+++ game/js/in_out.js
@@ -18,7 +18,7 @@ function revelar(){
 }
 
 function cadastrar(){
-    if(cadastro_nome.value.length > 2 && cadastro_email.value.endsWith('@hotmail.com') || cadastro_email.value.endsWith('@gmail.com') && cadastro_senha.value.length > 6 && cadastro_senha.value == conf_senha.value){
+    if(cadastro_nome.value.length > 2 && cadastro_email.value.endsWith('@gmail.com') && cadastro_senha.value.length >=6 && cadastro_senha.value == conf_senha.value){
         user_name += `${cadastro_nome.value}`;
         user_email +=  `${cadastro_email.value}`;
         user_password +=  `${cadastro_senha.value}`;
@@ -28,12 +28,32 @@ function cadastrar(){
         mostrar();
     }else{
         alert('Cadastro invalido!')
+        if(cadastro_nome.value.length <2){
+            lb_nome.innerHTML = 'Nick invalido';
+            lb_nome.style.display = 'block';
+            lb_cadastro_nome.style.display = 'none';
+            cadastro_nome.value = '';
+        }
+        if(cadastro_senha.value != conf_senha.value){
+            lb_senha.innerHTML = 'Senha incorreta';
+            lb_senha.style.display = 'block';
+            lb_cadastro_senha.style.display = 'none';
+            cadastro_senha.value = '';
+            conf_senha.value = '';
+        }else{
+            lb_email.innerHTML = 'Email incorreto';
+            lb_email.style.display = 'block';
+            lb_cadastro_email.style.display = 'none';
+            cadastro_email.value ='';
+        }
     }
 }
 
 function logar(){
     if(email.value == user_email && senha.value == user_password){
-        window.location.href = '../html/index.html'
+        window.location.href = 'http://www.clickjogos.com.br/jogos/naruto-fighting-cr-kakashi'
+        a_conta.style.display = 'none';
+        a_peril.style.display = 'block'
     }else{
         alert('Erro de validação!')
     }
